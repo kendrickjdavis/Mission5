3d5dcf9d27e843f39c7c9ecb07c9547ccfb5283a
kdavis5111 <119846859+kdavis5111@users.noreply.github.com>
2024-02-09 18:08:12 -0700

    Add project files.

---
+++ SQLGod/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculateTotal').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var rate = parseFloat($('#rate').val());
+
+        // Check if hours is a positive number
+        if (isNaN(hours) || hours <= 0) {
+            alert("Please enter a valid positive number for hours.");
+            return;
+        }
+
+        // Calculate total
+        var total = hours * rate;
+        var output = `$${total}`
+        $('#total').val(output);
+    });
+});
